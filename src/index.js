7d04f40cf96128fe4ec909a034dda725cca54760
fand <fand@gmork.in>
2015-07-21 17:22:03 +0900

    first

---
+++ src/index.js
@@ -0,0 +1,36 @@
+'use strict';
+import EventEmitter from 'events';
+
+const EVENT = (pageID) =>  'register:' + pageID;
+
+class Router extends EventEmitter {
+    constructor () {
+        super();
+        this.routes = {};
+    }
+
+    getController (pageID) {
+        return new Promise((resolve) => {
+            this.once(EVENT(pageID), () => {
+                this.routes[pageID](() => resolve(true));
+            });
+        });
+    }
+
+    selectRoute (pageID) {
+      var controller = this.getController(pageID);
+
+      if (this.routes[pageID]) {
+          this.emit(EVENT(pageID));
+      }
+
+      return controller();
+    }
+
+    register (pageID, controller) {
+        this.routes[pageID] = controller;
+        this.emit(EVENT(pageID));
+    }
+}
+
+export default Router;
